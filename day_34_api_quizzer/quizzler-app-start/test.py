560e3bcce5ec232c2bbce3fb2142b799e9faba98
Jimmy Liu <kuopingl+1@gmail.com>
2021-06-21 20:36:03 +0800

    update: fetch question_data from api

---
+++ day_34_api_quizzer/quizzler-app-start/test.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In World War II, Hawker Typhoons served in the Pacific theater.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The longest place named in the United States is Lake Chargoggagoggmanchauggagoggchaubunagungamaugg, located near Webster, MA.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The anime \"Lucky Star\" follows the story of one girl who is unaware she is God.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Solid Snake is actually a clone from the DNA of Big Boss in the Metal Gear Solid series' history.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "Rannamaari was a sea demon that haunted the people of the Maldives and had to be appeased monthly with the sacrifice of a virgin girl.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Comics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In the webcomic Homestuck, the first character introduced is Dave Strider.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Vehicles",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Ferrari has never made a V10 engine for any of its cars.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Mortal Kombat was almost based on Jean-Claude Van Damme movie.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "L'H\u00f4pital was the mathematician who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Art",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The Statue of Liberty's official name is \u201cLiberty Enlightening the World\u201d.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    }
+]
