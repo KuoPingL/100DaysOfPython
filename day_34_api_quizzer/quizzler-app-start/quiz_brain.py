23a8abf51cdc0397d59ab5490911a3ca93a7fce5
Jimmy Liu <kuopingl+1@gmail.com>
2021-06-24 21:52:11 +0800

    mod: Fetch Questions within QuizBrain
    In order to restart the game from QuizBrain, the question list will not be injected into QuizBrain via constructor.
    
    Instead, QuizBrain will call out to get_new_questions() from data.py.

--- day_34_api_quizzer/quizzler-app-start/quiz_brain.py
+++ day_34_api_quizzer/quizzler-app-start/quiz_brain.py
@@ -1,27 +1,42 @@
+from data import get_new_questions
+
+
 class QuizBrain:
 
-    def __init__(self, q_list):
+    def __init__(self):
         self.question_number = 0
         self.score = 0
-        self.question_list = q_list
+        self.question_list = get_new_questions()
         self.current_question = None
 
-    def still_has_questions(self):
+    def still_has_questions(self) -> bool:
         return self.question_number < len(self.question_list)
 
-    def next_question(self):
+    def next_question(self) -> str:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
-        self.check_answer(user_answer)
+        # user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
+        # self.check_answer(user_answer)
+        return self.current_question.text
 
-    def check_answer(self, user_answer):
+    def check_answer(self, user_answer) -> bool:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            # print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            # print("That's wrong.")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        # print(f"Your current score is: {self.score}/{self.question_number}")
+        # print("\n")
+
+    def start_new_game(self):
+        self.question_list = get_new_questions()
+        self.reset()
+
+    def reset(self):
+        self.score = 0
+        self.question_number = 0
+        self.current_question = None
