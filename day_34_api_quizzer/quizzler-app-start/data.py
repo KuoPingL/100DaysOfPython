dcca853edcf887360baf925e4e3124ead9ea6171
Jimmy Liu <kuopingl+1@gmail.com>
2021-06-24 21:51:37 +0800

    mod: add get_new_questions() -> list

--- day_34_api_quizzer/quizzler-app-start/data.py
+++ day_34_api_quizzer/quizzler-app-start/data.py
@@ -1,6 +1,17 @@
 from questions_fetcher import QuestionsFetcher
+from question_model import Question
 
 question_data = QuestionsFetcher.prepare_10_questions()
+
+
+def get_new_questions() -> list:
+    question_bank = []
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
+    return question_bank
 
 # question_data = [
 #     {
